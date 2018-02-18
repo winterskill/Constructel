8c0134e938a9f3096b9ac33b23dddda87e93899b
Winterskill <kowalski.hanguscorporation@gmail.com>
2018-02-19 00:34:09 +0100

    Ajout du look&feel
    Définition du look&feel pour qu'il colle à celui de l'OS.

--- src/darksidegames/constructel/Main.java
+++ src/darksidegames/constructel/Main.java
@@ -1,6 +1,15 @@
 package darksidegames.constructel;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 public class Main {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println("ERROR! COULD NOT LOAD SYSTEM LOOK AND FEEL!");
+			JOptionPane.showMessageDialog(null, "COULD NOT LOAD SYSTEM LOOK AND FEEL!", "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
