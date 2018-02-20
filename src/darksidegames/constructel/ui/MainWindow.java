2c60154ea4a7d7413a3ef51a5b32f01b03d12b1d
Winterskill <kowalski.hanguscorporation@gmail.com>
2018-02-20 01:16:51 +0100

    Passage de la classe darksidegames.constructel.ui.MainWindow en final

--- src/darksidegames/constructel/ui/MainWindow.java
+++ src/darksidegames/constructel/ui/MainWindow.java
@@ -8,12 +8,13 @@ import darksidegames.constructel.Settings;
  * La classe de la fenêtre principale de l'application.
  * @author Winterskill
  */
-public class MainWindow extends JFrame {
+public final class MainWindow extends JFrame {
 	public MainWindow() {
 		this.setTitle(Settings.title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setSize(Settings.defaultWidth, Settings.defaultHeight);
+		this.setResizable(true);
 		
 		this.setVisible(true);
 	}
